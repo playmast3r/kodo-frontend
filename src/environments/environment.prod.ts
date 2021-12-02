cac0947dc90f8a8f4b5e18c99687d2730db43ef8
Siddharth <siddharth.mahajan@jai-kisan.com>
2021-12-02 16:13:59 +0530

    heroku changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:3002/'
+  baseUrl: 'https://kodo-backend.herokuapp.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:3002'
+  baseUrl: 'https://kodo-backend.herokuapp.com'
 };
 
 /*
